6ff59101114034a3b926c12ba22fedc6ab5c468f
lridhiwan <155439121+lridhiwan@users.noreply.github.com>
2024-10-25 19:44:46 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,12 +1,12 @@
 const settings = {
   packname: 'My Sticker Pack',
-  author: 'L.Ridhiwan',
+  author: 'L.Ridhiwan powered by Rahmoon Communications Ug ',
   botName: "BOT 01",
-  botOwner: 'L.Ridhiwan', // Your name
+  botOwner: 'L.Ridhiwan powered by Rahmoon Communications Ug', // Your name
   ownerNumber: '256764837096', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
-  description: "Whatsapp bot created by L.Ridhiwan powered by L-SIX Communications.",
+  description: "Whatsapp bot created by L.Ridhiwan powered by Rahmoon Communications.",
   version: "1.0.0",
 };
 
